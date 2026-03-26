d83cb8a56732ea4e97c52914bbbeffcfbdc11288
Kushal Agrawal <84277789+KushalAgrawal30@users.noreply.github.com>
2026-03-26 06:04:29 +0530

    testing for errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const PORT = 3000
 const app = express()
 const server = createServer(app)
 
-const io = new Server(server, {
+io = Server(server, {
     cors:{
     origin:"http://localhost:5173",
     methods: ["GET","POST"],
